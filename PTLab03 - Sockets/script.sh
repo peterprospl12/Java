51c3a17fd27d17ae1bfb6c4c8dd0622d91ba5c80
piotreczek431 <48958687+peterprospl12@users.noreply.github.com>
2024-03-20 14:34:02 +0100

    Update script.sh

--- PTLab03 - Sockets/script.sh
+++ PTLab03 - Sockets/script.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-for i in {1..4}
+
+terminal_width=80
+terminal_height=24
+spacing=300
+
+gnome-terminal --geometry=${terminal_width}x${terminal_height}+0+1200 -- bash -c "java -cp target/classes org.example.Server; exec bash"
+
+sleep 1
+
+x_pos=0
+y_pos=0
+terminal_width=40
+
+for i in {0..4}
 do
-	gnome-terminal -- bash -c "java -cp target/classes org.example.Client; exec bash"
+    gnome-terminal --geometry=${terminal_width}x${terminal_height}+${x_pos}+${y_pos} -- bash -c "java -cp target/classes org.example.Client; exec bash" &
+    x_pos=$((x_pos + terminal_width + spacing))
 done
+
